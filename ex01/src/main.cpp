e40d9f800b5634cb485d80845c77422e3b813174
danfern3 <danfern3@student.42madrid.com>
2026-01-13 13:30:12 +0100

    wip: trying to fix compilation error (main tests could be the source of the problem)

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -2,6 +2,8 @@
 // #include "whatever.hpp"
 #include <iostream>
 
+const int MAX_LENGTH = 10;
+
 template <typename T>
 void		swap(T &a, T &b)
 {
@@ -20,9 +22,23 @@ T const&	max(T const&a, T const&b)
 	return a > b ? a : b;
 }
 
+template <typename T>
+void	iter(T const&arr, const int len, void (*f)(T const&))
+{
+	for (int i = 0; i < len; ++i)
+		f(arr[i]);
+}
+
+template <typename T>
+void	print_value(T const&val)
+{
+	std::cout << __func__ << ": " << val << "\n";
+}
+
 int main( void ) {
 	int a = 2;
 	int b = 3;
+	std::cout << YELLOW "--------------Template tests--------------\n" RESET;
 	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
@@ -33,5 +49,8 @@ int main( void ) {
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << YELLOW "--------------Template tests--------------\n" RESET;
+	int arr[MAX_LENGTH] = {1,2,3,4,5,6,7,8,9};
+	iter(arr, MAX_LENGTH, print_value);
 	return 0;
 }

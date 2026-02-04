cb20f9ce752414b759b09948e7b674701f633c3b
danfern3 <danfern3@student.42madrid.com>
2026-02-04 12:15:21 +0100

    fix: move templates definitions to header file

--- ex01/inc/iter.hpp
+++ ex01/inc/iter.hpp
@@ -2,34 +2,73 @@
 #ifndef __ITER_HPP__
 #define __ITER_HPP__
 
-template <typename T>
-void		swap(T &a, T &b);
+#include <iostream>
 
 template <typename T>
-T const&	min(T const&a, T const&b);
+void		swap(T &a, T &b)
+{
+	T	tmp = a;
+	a = b;
+	b = tmp;
+}
+template <typename T>
+T const&	min(T const&a, T const&b)
+{
+	return a < b ? a : b;
+}
+template <typename T>
+T const&	max(T const&a, T const&b)
+{
+	return a > b ? a : b;
+}
 
 template <typename T>
-T const&	max(T const&a, T const&b);
+void	automate(T a, T b)
+{
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << "\n";
+	std::cout << "min( a, b ) = " << ::min( a, b ) << "\n";
+	std::cout << "max( a, b ) = " << ::max( a, b ) << "\n\n";
+}
 
 template <typename T>
-void	automate(T a, T b);
+void	iter(T const *arr, const int len, void (*f)(T const&))
+{
+	for (int i = 0; i < len; ++i)
+		f(arr[i]);
+}
 
 template <typename T>
-void	iter(T const *arr, const int len, void (*f)(T const&));
+void	iter(T *arr, const int len, void (*f)(T &))
+{
+	for (int i = 0; i < len; ++i)
+		f(arr[i]);
+}
 
 template <typename T>
-void	iter(T *arr, const int len, void (*f)(T &));
+void	modValue(T &val)
+{
+	++val *= 2;
+	// toupper(val);
+}
 
 template <typename T>
-void	sum_value(T &val);
+void	noModValue(T const&val)
+{
+	std::cout << val << " ";
+}
 
 template <typename T>
-void	print_value(T const&val);
+void	automateRef(T *arr, const int len, void(*f)(T &))
+{
+	iter(arr, len, f);
+}
 
 template <typename T>
-void	automateRef(T *arr, const int len, void(*f)(T &));
-
-template <typename T>
-void	automateConstRef(T const *arr, const int len, void (*f)(T const&));
+void	automateConstRef(T const *arr, const int len, void (*f)(T const&))
+{
+	iter(arr, len, f);
+	std::cout << "\n";
+}
 
 #endif // __ITER_HPP__
--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -5,72 +5,72 @@
 
 const int MAX_LENGTH = 10;
 
-template <typename T>
-void		swap(T &a, T &b)
-{
-	T	tmp = a;
-	a = b;
-	b = tmp;
-}
-template <typename T>
-T const&	min(T const&a, T const&b)
-{
-	return a < b ? a : b;
-}
-template <typename T>
-T const&	max(T const&a, T const&b)
-{
-	return a > b ? a : b;
-}
+// template <typename T>
+// void		swap(T &a, T &b)
+// {
+// 	T	tmp = a;
+// 	a = b;
+// 	b = tmp;
+// }
+// template <typename T>
+// T const&	min(T const&a, T const&b)
+// {
+// 	return a < b ? a : b;
+// }
+// template <typename T>
+// T const&	max(T const&a, T const&b)
+// {
+// 	return a > b ? a : b;
+// }
 
-template <typename T>
-void	automate(T a, T b)
-{
-	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << "\n";
-	std::cout << "min( a, b ) = " << ::min( a, b ) << "\n";
-	std::cout << "max( a, b ) = " << ::max( a, b ) << "\n\n";
-}
+// template <typename T>
+// void	automate(T a, T b)
+// {
+// 	::swap( a, b );
+// 	std::cout << "a = " << a << ", b = " << b << "\n";
+// 	std::cout << "min( a, b ) = " << ::min( a, b ) << "\n";
+// 	std::cout << "max( a, b ) = " << ::max( a, b ) << "\n\n";
+// }
 
-template <typename T>
-void	iter(T const *arr, const int len, void (*f)(T const&))
-{
-	for (int i = 0; i < len; ++i)
-		f(arr[i]);
-}
+// template <typename T>
+// void	iter(T const *arr, const int len, void (*f)(T const&))
+// {
+// 	for (int i = 0; i < len; ++i)
+// 		f(arr[i]);
+// }
 
-template <typename T>
-void	iter(T *arr, const int len, void (*f)(T &))
-{
-	for (int i = 0; i < len; ++i)
-		f(arr[i]);
-}
+// template <typename T>
+// void	iter(T *arr, const int len, void (*f)(T &))
+// {
+// 	for (int i = 0; i < len; ++i)
+// 		f(arr[i]);
+// }
 
-template <typename T>
-void	modValue(T &val)
-{
-	++val *= 2;
-	// toupper(val);
-}
+// template <typename T>
+// void	modValue(T &val)
+// {
+// 	++val *= 2;
+// 	// toupper(val);
+// }
 
-template <typename T>
-void	noModValue(T const&val)
-{
-	std::cout << val << " ";
-}
+// template <typename T>
+// void	noModValue(T const&val)
+// {
+// 	std::cout << val << " ";
+// }
 
-template <typename T>
-void	automateRef(T *arr, const int len, void(*f)(T &))
-{
-	iter(arr, len, f);
-}
+// template <typename T>
+// void	automateRef(T *arr, const int len, void(*f)(T &))
+// {
+// 	iter(arr, len, f);
+// }
 
-template <typename T>
-void	automateConstRef(T const *arr, const int len, void (*f)(T const&))
-{
-	iter(arr, len, f);
-	std::cout << "\n";
-}
+// template <typename T>
+// void	automateConstRef(T const *arr, const int len, void (*f)(T const&))
+// {
+// 	iter(arr, len, f);
+// 	std::cout << "\n";
+// }
 
 int main( void ) {
 	std::cout << YELLOW "--------------Template tests--------------\n" RESET;
@@ -79,7 +79,7 @@ int main( void ) {
 	automate(20.00, 30.50);
 	automate("chain1", "chain2");
 	automate('3', 'X');
-	std::cout << YELLOW "--------------Template tests--------------\n" RESET;
+	std::cout << YELLOW "--------------Iter tests--------------\n" RESET;
 
 	int arr[MAX_LENGTH] = {1,2,3,4,5,6,7,8,9,10};
 	automateConstRef(arr, MAX_LENGTH, noModValue);

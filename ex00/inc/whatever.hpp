00eae3257cf83f333c4df41d4cf1612b33b3a087
danfern3 <danfern3@student.42madrid.com>
2026-02-04 12:04:38 +0100

    fix: move templates definitions to header file

--- ex00/inc/whatever.hpp
+++ ex00/inc/whatever.hpp
@@ -6,15 +6,30 @@
 #include "colors.hpp"
 
 template <typename T>
-void		swap(T &a, T &b);
+void		swap(T &a, T &b)
+{
+	T	tmp = a;
+	a = b;
+	b = tmp;
+}
+template <typename T>
+T const&	min(T const&a, T const&b)
+{
+	return a < b ? a : b;
+}
+template <typename T>
+T const&	max(T const&a, T const&b)
+{
+	return a > b ? a : b;
+}
 
 template <typename T>
-T const&	min(T const&a, T const&b);
-
-template <typename T>
-T const&	max(T const&a, T const&b);
-
-template <typename T>
-void	automate(T a, T b);
+void	automate(T a, T b)
+{
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << "\n";
+	std::cout << "min( a, b ) = " << ::min( a, b ) << "\n";
+	std::cout << "max( a, b ) = " << ::max( a, b ) << "\n\n";
+}
 
 #endif // __WHATEVER_HPP__
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -3,33 +3,6 @@
 #include "whatever.hpp"
 #include <iostream>
 
-template <typename T>
-void		swap(T &a, T &b)
-{
-	T	tmp = a;
-	a = b;
-	b = tmp;
-}
-template <typename T>
-T const&	min(T const&a, T const&b)
-{
-	return a < b ? a : b;
-}
-template <typename T>
-T const&	max(T const&a, T const&b)
-{
-	return a > b ? a : b;
-}
-
-template <typename T>
-void	automate(T a, T b)
-{
-	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << "\n";
-	std::cout << "min( a, b ) = " << ::min( a, b ) << "\n";
-	std::cout << "max( a, b ) = " << ::max( a, b ) << "\n\n";
-}
-
 int main( void ) {
 
 	::automate(2, 3);
